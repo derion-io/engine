db60543b44f7d605ca15fe641d68671fb2f55159
athevinha <nguyenthevinh1002@gmail.com>
2024-10-08 14:11:25 +0700

    build

---
+++ dist/services/aggregator.d.ts
@@ -0,0 +1,24 @@
+import { Profile } from './../profile';
+import { Contract, ethers } from 'ethers';
+import { JsonRpcProvider } from '@ethersproject/providers';
+import { IEngineConfig } from '../utils/configs';
+import { SwapAndOpenAggregatorType, rateDataAggregatorType } from '../types';
+export declare class Aggregator {
+    account?: string;
+    provider: ethers.providers.JsonRpcProvider;
+    overrideProvider: JsonRpcProvider;
+    signer?: ethers.providers.JsonRpcSigner;
+    config: IEngineConfig;
+    paraDataBaseURL: string;
+    paraBuildTxBaseURL: string;
+    paraDataBaseVersion: string;
+    constructor(config: IEngineConfig, profile: Profile, paraDataBaseURL?: string, paraBuildTxBaseURL?: string, paraVersion?: string);
+    getRateAndBuildTxSwapApi(getRateData: rateDataAggregatorType, openData: SwapAndOpenAggregatorType, helperOverride?: Contract, slippage?: number): Promise<{
+        rateData: any;
+        swapData: any;
+        openTx: any;
+    }>;
+    getRate(getRateData: rateDataAggregatorType): Promise<any>;
+    buildTx(getRateData: rateDataAggregatorType, rateData: any, slippage?: number): Promise<any>;
+    private generateSigner;
+}
