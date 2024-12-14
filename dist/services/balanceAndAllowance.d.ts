7bf10031d4fc4528d5c237bbda1dbaf273da95e5
Zergity <zergity@gmail.com>
2024-12-14 23:32:09 +0700

    build

---
+++ dist/services/balanceAndAllowance.d.ts
@@ -0,0 +1,25 @@
+import { BigNumber } from 'ethers';
+import { AllowancesType, BalancesType, MaturitiesType } from '../types';
+import { IEngineConfig } from '../utils/configs';
+import { JsonRpcProvider } from '@ethersproject/providers';
+import { Profile } from '../profile';
+import { Resource } from './resource';
+export declare function keyFromTokenId(id: BigNumber): string;
+export type BnAReturnType = {
+    chainId: number;
+    account: string;
+    balances: BalancesType;
+    allowances: AllowancesType;
+    maturities: MaturitiesType;
+};
+export declare class BnA {
+    provider: JsonRpcProvider;
+    rpcUrl: string;
+    bnAAddress: string;
+    profile: Profile;
+    RESOURCE: Resource;
+    constructor(config: IEngineConfig & {
+        RESOURCE: Resource;
+    }, profile: Profile);
+    getBalanceAndAllowance(account: string, withNative?: boolean): Promise<BnAReturnType>;
+}
