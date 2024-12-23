fcf1a3ebea67361fe08702abb75d529f42c24a55
Zergity <zergity@gmail.com>
2024-12-23 15:18:29 +0700

    lock the account logs

--- test/sdk.test.ts
+++ test/sdk.test.ts
@@ -1,48 +1,31 @@
 import { DerionSDK } from '../src/sdk'
-import { groupBy, packPosId, throwError } from '../src/utils'
+import { packPosId, throwError } from '../src/utils'
 import { Interceptor } from './shared/libs/interceptor'
-import { AssistedJsonRpcProvider } from 'assisted-json-rpc-provider'
-import { hexZeroPad } from 'ethers/lib/utils'
 import { JsonRpcProvider } from '@ethersproject/providers'
 import { NATIVE_ADDRESS, POOL_IDS } from '../src/utils/constant'
 import { numberToWei } from '../src/utils/helper'
 import { VoidSigner } from 'ethers'
 import { formatPositionView } from '../src/utils/positions'
+import path from 'path'
+import { LogType } from '../src/type'
+
 const interceptor = new Interceptor()
+
 describe('SDK', () => {
   beforeEach(() => {
     interceptor.setContext(expect.getState().currentTestName)
   })
 
-  test('postions', async () => {
+  test('logs', async () => {
     const chainId = 137
     const accountAddress = '0xE61383556642AF1Bd7c5756b13f19A63Dc8601df'
 
     const rpcUrl = process.env['RPC_' + chainId] ?? throwError()
-    const scanApi = process.env['SCAN_API_' + chainId] ?? throwError()
 
     const sdk = new DerionSDK({ chainId })
     await sdk.init()
 
-    const provider = new AssistedJsonRpcProvider(
-      new JsonRpcProvider(rpcUrl), {
-      url: scanApi,
-      apiKeys: process.env['SCAN_API_KEY_' + chainId]?.split(',') ?? throwError(),
-    }
-    )
-    const accTopic = hexZeroPad(accountAddress, 32)
-    const logs = await provider.getLogs({
-      fromBlock: 0,
-      toBlock: Number.MAX_SAFE_INTEGER,
-      topics: [
-        [],
-        [null, accTopic],
-        [null, null, accTopic],
-        [null, null, null, accTopic],
-      ],
-    })
-
-    const txLogs = Object.values(groupBy(logs, 'transactionHash'))
+    const txLogs = await loadAccountLogs(rpcUrl, chainId, accountAddress)
     const { poolAddresses } = sdk.extractLogs(txLogs)
 
     const stateLoader = sdk.getStateLoader(rpcUrl)
@@ -57,35 +40,17 @@ describe('SDK', () => {
 
     console.log(...posViews.map(pv => formatPositionView(pv)))
   })
+
   test('native-open', async () => {
     const chainId = 42161
     const accountAddress = '0xD42d6d58F95A3DA9011EfEcA086200A64B266c10'
     const rpcUrl = process.env['RPC_' + chainId] ?? throwError()
-    const scanApi = process.env['SCAN_API_' + chainId] ?? throwError()
     const sdk = new DerionSDK({ chainId })
     await sdk.init()
 
-    const provider = new AssistedJsonRpcProvider(
-      new JsonRpcProvider(rpcUrl), {
-      url: scanApi,
-      apiKeys: process.env['SCAN_API_KEY_' + chainId]?.split(',') ?? throwError(),
-    }
-    )
-    // 53501226
     const signer = new VoidSigner(accountAddress, new JsonRpcProvider(rpcUrl));
 
-    const accTopic = hexZeroPad(accountAddress, 32)
-    const logs = await provider.getLogs({
-      fromBlock: 0,
-      toBlock: Number.MAX_SAFE_INTEGER,
-      topics: [
-        [],
-        [null, accTopic],
-        [null, null, accTopic],
-        [null, null, null, accTopic],
-      ],
-    })
-    const txLogs = Object.values(groupBy(logs, 'transactionHash'))
+    const txLogs = await loadAccountLogs(rpcUrl, chainId, accountAddress)
     const poolAdrs = sdk.extractLogs(txLogs)
 
     const stateLoader = sdk.getStateLoader(rpcUrl)
@@ -129,36 +94,18 @@ describe('SDK', () => {
     expect(Number(amountOutsC)).toBeGreaterThan(0)
     expect(Number(gasUsedC)).toBeGreaterThan(0)
   })
+
   test('R-open', async () => {
     const chainId = 42161
     const accountAddress = '0xE61383556642AF1Bd7c5756b13f19A63Dc8601df'
     const poolToSwap = '0xf3cE4cbfF83AE70e9F76b22cd9b683F167d396dd'
     const rpcUrl = process.env['RPC_' + chainId] ?? throwError()
-    const scanApi = process.env['SCAN_API_' + chainId] ?? throwError()
     const sdk = new DerionSDK({ chainId })
     await sdk.init()
 
-    const provider = new AssistedJsonRpcProvider(
-      new JsonRpcProvider(rpcUrl), {
-      url: scanApi,
-      apiKeys: process.env['SCAN_API_KEY_' + chainId]?.split(',') ?? throwError(),
-    }
-    )
-    // 53501226
     const signer = new VoidSigner(accountAddress, new JsonRpcProvider(rpcUrl));
 
-    const accTopic = hexZeroPad(accountAddress, 32)
-    const logs = await provider.getLogs({
-      fromBlock: 0,
-      toBlock: Number.MAX_SAFE_INTEGER,
-      topics: [
-        [],
-        [null, accTopic],
-        [null, null, accTopic],
-        [null, null, null, accTopic],
-      ],
-    })
-    const txLogs = Object.values(groupBy(logs, 'transactionHash'))
+    const txLogs = await loadAccountLogs(rpcUrl, chainId, accountAddress)
     const poolAdrs = sdk.extractLogs(txLogs)
 
     const stateLoader = sdk.getStateLoader(rpcUrl)
@@ -205,35 +152,18 @@ describe('SDK', () => {
     expect(Number(amountROutsC)).toBeGreaterThan(0)
     expect(Number(gasUsedRC)).toBeGreaterThan(0)
   })
+
   test('any-open', async () => {
     const chainId = 42161
     const accountAddress = '0xD42d6d58F95A3DA9011EfEcA086200A64B266c10'
     const poolToSwap = '0xf3cE4cbfF83AE70e9F76b22cd9b683F167d396dd'
     const rpcUrl = process.env['RPC_' + chainId] ?? throwError()
-    const scanApi = process.env['SCAN_API_' + chainId] ?? throwError()
     const sdk = new DerionSDK({ chainId })
     await sdk.init()
 
-    const provider = new AssistedJsonRpcProvider(
-      new JsonRpcProvider(rpcUrl), {
-      url: scanApi,
-      apiKeys: process.env['SCAN_API_KEY_' + chainId]?.split(',') ?? throwError(),
-    }
-    )
     const signer = new VoidSigner(accountAddress, new JsonRpcProvider(rpcUrl));
 
-    const accTopic = hexZeroPad(accountAddress, 32)
-    const logs = await provider.getLogs({
-      fromBlock: 0,
-      toBlock: Number.MAX_SAFE_INTEGER,
-      topics: [
-        [],
-        [null, accTopic],
-        [null, null, accTopic],
-        [null, null, null, accTopic],
-      ],
-    })
-    const txLogs = Object.values(groupBy(logs, 'transactionHash'))
+    const txLogs = await loadAccountLogs(rpcUrl, chainId, accountAddress)
     const poolAdrs = sdk.extractLogs(txLogs)
 
     const stateLoader = sdk.getStateLoader(rpcUrl)
@@ -275,33 +205,16 @@ describe('SDK', () => {
     expect(Number(amountOutsUSDCC)).toBeGreaterThan(0)
     expect(Number(gasUsedUSDCC)).toBeGreaterThan(0)
   })
+
   test('positions-swap', async () => {
     const chainId = 42161
     const accountAddress = '0xD42d6d58F95A3DA9011EfEcA086200A64B266c10'
     const rpcUrl = process.env['RPC_' + chainId] ?? throwError()
-    const scanApi = process.env['SCAN_API_' + chainId] ?? throwError()
     const sdk = new DerionSDK({ chainId })
     await sdk.init()
-    const provider = new AssistedJsonRpcProvider(
-      new JsonRpcProvider(rpcUrl), {
-      url: scanApi,
-      apiKeys: process.env['SCAN_API_KEY_' + chainId]?.split(',') ?? throwError(),
-    }
-    )
     const signer = new VoidSigner(accountAddress, new JsonRpcProvider(rpcUrl));
 
-    const accTopic = hexZeroPad(accountAddress, 32)
-    const logs = await provider.getLogs({
-      fromBlock: 0,
-      toBlock: Number.MAX_SAFE_INTEGER,
-      topics: [
-        [],
-        [null, accTopic],
-        [null, null, accTopic],
-        [null, null, null, accTopic],
-      ],
-    })
-    const txLogs = Object.values(groupBy(logs, 'transactionHash'))
+    const txLogs = await loadAccountLogs(rpcUrl, chainId, accountAddress)
     const poolAdrs = sdk.extractLogs(txLogs)
 
     const stateLoader = sdk.getStateLoader(rpcUrl)
@@ -345,33 +258,16 @@ describe('SDK', () => {
     expect(Number(amountOutsBC)).toBeGreaterThan(0)
     expect(Number(gasUsedBC)).toBeGreaterThan(0)
   })
+
   test('positions-close', async () => {
     const chainId = 42161
     const accountAddress = '0xD42d6d58F95A3DA9011EfEcA086200A64B266c10'
     const rpcUrl = process.env['RPC_' + chainId] ?? throwError()
-    const scanApi = process.env['SCAN_API_' + chainId] ?? throwError()
     const sdk = new DerionSDK({ chainId })
     await sdk.init()
-    const provider = new AssistedJsonRpcProvider(
-      new JsonRpcProvider(rpcUrl), {
-      url: scanApi,
-      apiKeys: process.env['SCAN_API_KEY_' + chainId]?.split(',') ?? throwError(),
-    }
-    )
     const signer = new VoidSigner(accountAddress, new JsonRpcProvider(rpcUrl));
 
-    const accTopic = hexZeroPad(accountAddress, 32)
-    const logs = await provider.getLogs({
-      fromBlock: 0,
-      toBlock: Number.MAX_SAFE_INTEGER,
-      topics: [
-        [],
-        [null, accTopic],
-        [null, null, accTopic],
-        [null, null, null, accTopic],
-      ],
-    })
-    const txLogs = Object.values(groupBy(logs, 'transactionHash'))
+    const txLogs = await loadAccountLogs(rpcUrl, chainId, accountAddress)
     const poolAdrs = sdk.extractLogs(txLogs)
 
     const stateLoader = sdk.getStateLoader(rpcUrl)
@@ -508,3 +404,8 @@ describe('SDK', () => {
 
   })
 })
+
+async function loadAccountLogs(rpcUrl, chainId, accountAddress): Promise<LogType[][]> {
+  const fp = path.join(__dirname, `logs/${chainId}-${accountAddress}.json`)
+  return require(fp)
+}
