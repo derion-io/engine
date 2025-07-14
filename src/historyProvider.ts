431508ee13cd6d8938db61633e3be299f30acb52
athevinha <nguyenthevinh1002@gmail.com>
2025-07-14 17:18:41 +0700

    history provider

--- src/historyProvider.ts
+++ src/historyProvider.ts
@@ -49,6 +49,13 @@ type GetPricesType = {
   data: { [key: string]: string }
   code: number
 }
+export const INTERVAL_TO_GECKO: {[key:string]: {timeframe: string, aggregate: number}} = {
+  '5m': { timeframe: 'minute', aggregate: 5 },
+  '15m': { timeframe: 'minute', aggregate: 15 },
+  '1H': { timeframe: 'hour', aggregate: 1 },
+  '4H': { timeframe: 'hour', aggregate: 4 },
+  '1D': { timeframe: 'day', aggregate: 1 },
+};
 
 export default {
   history: history,
@@ -56,39 +63,51 @@ export default {
   getBars: async function ({
     route,
     resolution,
-    inputToken,
-    outputToken,
+    // inputToken,
+    // outputToken,
     limit,
     chainId,
-    to,
-    barValueType,
+    gtId,
+    // to,
+    // barValueType,
   }: {
-    inputToken: TokenType
-    outputToken: TokenType
+    inputToken?: TokenType
+    outputToken?: TokenType
     route: string
     resolution: string
     limit: number
     chainId: string
+    gtId?: string
     to: number
     barValueType?: 'string'
   }): Promise<CandleType[]> {
     const q = route.split('/').join(',')
-    const url = `${CHART_API_ENDPOINT.replaceAll('{chartId}', chainId)}candleline4?q=${q}&r=${convertResolution(
-      resolution,
-    )}&l=${limit}&t=${to}`
+    const intervalConf = INTERVAL_TO_GECKO[resolution];
+    const url = `https://api.geckoterminal.com/api/v2/networks/${gtId || chainId}/pools/${route.split(',')[1]}/ohlcv/${intervalConf.timeframe}?aggregate=${intervalConf.aggregate}&include_empty_intervals=false&limit=${limit || 300}`;
+    // const url = `${CHART_API_ENDPOINT.replaceAll('{chartId}', chainId)}candleline4?q=${q}&r=${convertResolution(
+    //   resolution,
+    // )}&l=${limit}&t=${to}`
+    console.log(url)
 
     const response = await fetch(url).then((r: any) => r.json())
-    if (response && response.s === 'ok' && response.t && response.t.length > 0) {
+    // console.log(response, limit, chainId, to, barValueType)
+    if (response && response?.data && response?.data?.attributes?.ohlcv_list?.length > 0 ) {
       const bars: CandleType[] = []
-      const decimals = 18 + (outputToken?.decimals || 18) - (inputToken?.decimals || 18)
-      for (let i = 0; i < response.t.length; i++) {
+      const candles:number[][] = response?.data?.attributes?.ohlcv_list
+      
+      for (let i = 0; i < candles.length; i++) {
         bars.push({
-          low: formatResult(weiToNumber(numberToWei(response.l[i]), decimals), barValueType),
-          open: formatResult(weiToNumber(numberToWei(response.o[i]), decimals), barValueType),
-          time: response.t[i] * 1000,
-          volume: formatResult(weiToNumber(response.v[i].split('.')[0], outputToken?.decimals), barValueType),
-          close: formatResult(weiToNumber(numberToWei(response.c[i]), decimals), barValueType),
-          high: formatResult(weiToNumber(numberToWei(response.h[i]), decimals), barValueType),
+          low: candles[i][3],
+          // formatResult(weiToNumber(numberToWei(response.l[i]), decimals), barValueType),
+          open: candles[i][1],
+          // formatResult(weiToNumber(numberToWei(response.o[i]), decimals), barValueType),
+          time: candles[i][1] * 1000,
+          volume: candles[i][5],
+          // formatResult(weiToNumber(response.v[i].split('.')[0], outputToken?.decimals), barValueType),
+          close: candles[i][4],
+          // formatResult(weiToNumber(numberToWei(response.c[i]), decimals), barValueType),
+          high: candles[i][2],
+          // formatResult(weiToNumber(numberToWei(response.h[i]), decimals), barValueType),
         })
       }
       return bars
